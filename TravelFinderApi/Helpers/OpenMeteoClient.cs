2c8ea48484f24a307f9d6e1e710bfaaceca62d84
Sourav Mondal <smondol@insightintechnology.com>
2025-11-29 13:42:28 +0600

    gracefully-error-handling-on-recommedation-api

--- TravelFinderApi/Helpers/OpenMeteoClient.cs
+++ TravelFinderApi/Helpers/OpenMeteoClient.cs
@@ -18,8 +18,7 @@ namespace TravelFinderApi.Helpers
                  : $"start_date={startDate?.ToString("yyyy-MM-dd")}&end_date={startDate?.AddDays(days - 1).ToString("yyyy-MM-dd")}")}" +
              $"&hourly=temperature_2m&timezone=auto";
 
-            var response = await _httpClient.GetStringAsync(url);
-            return response;
+            return await SafeGetAsync(url);
         }
         public async Task<string> GetMultiLocationAirQuality(bool isRanking, string latitudes, string longitudes, DateTime? startDate = null, int days = 7)
         {
@@ -29,8 +28,26 @@ namespace TravelFinderApi.Helpers
                  ? $"forecast_days={days}"
                  : $"start_date={startDate?.ToString("yyyy-MM-dd")}&end_date={startDate?.AddDays(days - 1).ToString("yyyy-MM-dd")}")}" +
                 $"&hourly=pm2_5&timezone=auto";
-            var response = await _httpClient.GetStringAsync(url);
-            return response;
+
+            return await SafeGetAsync(url);
         }
+
+        private async Task<string> SafeGetAsync(string url)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync(url);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return $"success: false, error: {response.StatusCode} : {response.ReasonPhrase}";
+                }
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                return $"success: false, error: Unexpected error: {ex.Message}";
+            }
+        }
+
     }
 }
